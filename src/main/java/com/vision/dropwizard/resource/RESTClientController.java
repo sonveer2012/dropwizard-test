8803a913136eeac0456790ba121e778ca6e1d398
sonveer-narwaria <sonveer.narwaria@flipkart.com>
2019-10-06 19:36:25 +0530

    Add auth

--- src/main/java/com/vision/dropwizard/resource/RESTClientController.java
+++ src/main/java/com/vision/dropwizard/resource/RESTClientController.java
@@ -4,10 +4,8 @@ package com.vision.dropwizard.resource;
  * @author sonveer.narwaria on 06/10/19
  * @project dropwizard-pipeline
  */
-import com.vision.dropwizard.model.Employee;
-import com.vision.dropwizard.services.EmployeeDB;
 
-import java.util.ArrayList;
+import com.vision.dropwizard.model.Employee;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -18,13 +16,13 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 
 import static com.vision.dropwizard.utils.Constants.PORT;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/client/")
-public class RESTClientController
-{
+public class RESTClientController {
     private Client client;
 
     public RESTClientController(Client client) {
@@ -33,11 +31,10 @@ public class RESTClientController
 
     @GET
     @Path("/employees/")
-    public Response getEmployees()
-    {
+    public Response getEmployees() {
         //Do not hard code in your application
-        WebTarget webTarget = client.target("http://localhost:"+PORT+"/employees");
-        Invocation.Builder invocationBuilder =  webTarget.request(MediaType.APPLICATION_JSON);
+        WebTarget webTarget = client.target("http://localhost:" + PORT + "/employees");
+        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
         Response response = invocationBuilder.get();
         @SuppressWarnings("rawtypes")
         ArrayList employees = response.readEntity(ArrayList.class);
@@ -46,11 +43,10 @@ public class RESTClientController
 
     @GET
     @Path("/employees/{id}")
-    public Response getEmployeeById(@PathParam("id") int id)
-    {
+    public Response getEmployeeById(@PathParam("id") int id) {
         //Do not hard code in your application
-        WebTarget webTarget = client.target("http://localhost:"+PORT+"/employees"+id);
-        Invocation.Builder invocationBuilder =  webTarget.request(MediaType.APPLICATION_JSON);
+        WebTarget webTarget = client.target("http://localhost:" + PORT + "/employees" + id);
+        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
         Response response = invocationBuilder.get();
         Employee employee = response.readEntity(Employee.class);
         return Response.ok(employee).build();
