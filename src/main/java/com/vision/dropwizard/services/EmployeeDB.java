f0bc9e3a1915670548b816be7d7411ef1d0bf547
sonveer-narwaria <sonveer.narwaria@flipkart.com>
2019-10-08 21:01:25 +0530

    Change names

--- src/main/java/com/vision/dropwizard/services/EmployeeDB.java
+++ src/main/java/com/vision/dropwizard/services/EmployeeDB.java
@@ -18,7 +18,6 @@ public class EmployeeDB {
     static {
         employees.put(1, new Employee(1, "Sanjana", "Singh", "IND"));
         employees.put(2, new Employee(2, "Sapna", "Sharma", "IND"));
-        employees.put(3, new Employee(3, "Ankita", "Katare", "IND"));
         employees.put(4, new Employee(4, "Shakshi", "Bhadouriya", "IND"));
     }
 
